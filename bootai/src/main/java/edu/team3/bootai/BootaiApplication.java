5fb7a7d72a780eb531b57807ac8d828f40aa3da3
312-KimJungHyun <112wjdugs@gmail.com>
2025-01-23 11:29:41 +0900

    초기설정

---
+++ bootai/src/main/java/edu/team3/bootai/BootaiApplication.java
@@ -0,0 +1,13 @@
+package edu.team3.bootai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootaiApplication.class, args);
+	}
+
+}
